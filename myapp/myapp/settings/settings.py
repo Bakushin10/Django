409a50dca1fdf189c8da435686c2f7b745479e61
“YOUR <shnnagai@gmail.com>
2019-08-30 21:31:08 +0900

    remove static files

--- myapp/myapp/settings/settings.py
+++ myapp/myapp/settings/settings.py
@@ -20,12 +20,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@9n#4+m6ghf=10-9sr7+=z2l^0dq$!6kmd!)*384s@wkvoy-u4'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 # Application definition
 
 REST_FRAMEWORK = {
