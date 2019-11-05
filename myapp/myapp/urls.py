7d6d3a6bf8b49f745a5cef37d212b2a776843e43
“YOUR <shnnagai@gmail.com>
2019-11-05 23:26:47 +0900

    build(core) : create swagger for OpenAPIs doc

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -1,26 +1,15 @@
-"""myapp URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.urls import path, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 from ocr_api.views import post_dummy_data, get_dummy_data, post_ocr_results, get_ocr_results, get_ocred_image, get_ocr_results_by_id
 #from poll.urls import router as polls_router
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='API Lists')
 
 urlpatterns = [
+    path('swagger/', schema_view),
     path('admin/', admin.site.urls),
     path('poll/<slug:location>/', include('poll.urls')),
     path('weather/<slug:location>/', include('weather.urls')),
