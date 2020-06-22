b48464bcd10b1c2d48c7c2f75fb906b1a59fe1ee
Shin <nagai.shin1@aig.co.jp>
2020-06-22 17:20:26 +0900

    update script

--- dependency/dependency_installer.py
+++ dependency/dependency_installer.py
@@ -6,31 +6,31 @@ def install_dependencies():
     """
     installing the dependencies from 'dst'
     """
+    absolute_path_to_package = "/home/wasuser/.local/share/virtualenvs/api-veReZ-M2/lib/python3.7/site-packages/"
+    
     whl_path = dst + "*.whl"
     tar_gz_path = dst + "*.gz"
     print("serching .whl and .gz file in {}".format(dst))
     whl_file_list, tar_gz_file_list = get_all_package(whl_path, tar_gz_path)
 
-    # whl files
+    # install whl files
     for whl_file in whl_file_list:
         whl_file = whl_file.replace(" ", "")
         if os.path.isfile(whl_file):
             # install the dependencies
             whl_file = whl_file.replace("\\", "/")
             print("--- installing {}".format(whl_file))
-            install_command = f"{whl_file}  --no-index --find-links {whl_file}"
-            subprocess.check_call([sys.executable, "-m", "pip", "install",install_command])
-            #subprocess.check_call([sys.executable, "-m", "pip", install_command, whl_file])
+            subprocess.check_call([sys.executable, "-m", "pip", "install", whl_file, "--find-links", absolute_path_to_package, "--no-index"])
 
-    # tar_gz file
+    # install tar_gz file
     for gz_file in tar_gz_file_list:
         gz_file = gz_file.replace(" ", "")
         if os.path.isfile(gz_file):
             # install the dependencies
             gz_file = gz_file.replace("\\\\", "/")
             print("--- installing {}".format(gz_file))
-            install_command = f"install {gz_file}  --no-index --find-links {gz_file}"
-            subprocess.check_call([sys.executable, "-m", "pip", "install",install_command])
+            install_command = f"install {gz_file} --find-links {absolute_path_to_package} "
+            subprocess.check_call([sys.executable, "-m", "pip", "install", gz_file, "--find-links", absolute_path_to_package, "--no-index"])
 
 def get_all_package(whl_path, tar_gz_path):
     whl_files = glob.iglob(os.path.join(source_dir, whl_path))
